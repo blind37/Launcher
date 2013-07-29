c76663a60b2db1fce0acffd74356fb622fb4247d
littlecat1988 <137330543@qq.com>
2013-07-29 12:00:35 +0800

    log2

--- src/com/yao_guet/AllAppList.java
+++ src/com/yao_guet/AllAppList.java
@@ -51,7 +51,7 @@ public class AllAppList extends Activity implements AllAppsGridView.AllView {
             if (intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED)) {
                 getPageCount();
                 Log.e(TAG,"intent= "+intent);
-                String packgename = intent.getDataString().substring(8);
+                String packgename = intent.getDataString().substring(8);//lixiang
 
                 if(apps.size()==((pageCount-1)*APP_PAGE_SIZE+1)){
                  addChildView();
